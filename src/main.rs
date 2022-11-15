babdcc4600eebf0d0558d95b5054fdd9cd54cc33
chodkows <chodkows@gmail.com>
2022-11-15 22:33:53 +0100

    Blink with H bridge

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,18 @@ use ruduino::cores::current::{port};
 
 #[no_mangle]
 pub extern fn main() {
-    port::B5::set_output();
+    port::D6::set_output();
+    port::D6::set_high();
+
+    port::D7::set_output();
+    port::B0::set_output();
 
     loop {
-        port::B5::set_high();
-
-        ruduino::delay::delay_ms(1000);
-
-        port::B5::set_low();
-
+        port::D7::set_low();
+        port::B0::set_high();
+        ruduino::delay::delay_ms(3000);
+        port::D7::set_high();
+        port::B0::set_low();
         ruduino::delay::delay_ms(1000);
     }
 }
